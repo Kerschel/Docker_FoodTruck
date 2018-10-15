15ca030998f29804abc2d69d6869e15912815a31
Kerschel James <kerschels@hotmail.com>
2018-10-15 03:10:53 +0200

    changed keypair

--- setup-aws-ecs.sh
+++ setup-aws-ecs.sh
@@ -4,7 +4,7 @@
 ecs-cli configure --region us-east-2 --cluster foodtrucks
 
 # setup cloud formation template
-ecs-cli up --keypair kerschel-pair --capability-iam --size 2 --instance-type t2.micro
+ecs-cli up --keypair james-pair --capability-iam --size 2 --instance-type t2.micro
 
 # deploy
 ecs-cli compose --file aws-compose.yml up
