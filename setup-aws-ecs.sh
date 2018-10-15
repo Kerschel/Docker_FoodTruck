75f050895bbd31b91d4de6bda4ffc227ad5565e9
Kerschel James <kerschels@hotmail.com>
2018-10-15 02:58:00 +0200

    Docker Project files

--- setup-aws-ecs.sh
+++ setup-aws-ecs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # configure
-ecs-cli configure --region us-east-1 --cluster foodtrucks
+ecs-cli configure --region us-east-2 --cluster foodtrucks
 
 # setup cloud formation template
-ecs-cli up --keypair ecs --capability-iam --size 2 --instance-type t2.micro
+ecs-cli up --keypair kerschel-pair --capability-iam --size 2 --instance-type t2.micro
 
 # deploy
 ecs-cli compose --file aws-compose.yml up
